49f4770df03d065864b0b278b295592a1f16d6ed
nikolas.melo <nikolas.melo@autaza.com>
2023-09-26 15:20:28 -0300

    Feat: Extract video resolutions

--- views.py
+++ views.py
@@ -4,15 +4,22 @@ from flask import Blueprint, request, render_template, send_file
 
 views = Blueprint(__name__, "views")
 
-
 @views.route("/", methods=["GET", "POST"])
 def index():
+    video_url = ''
+    resolutions = []
     if request.method == "POST":
         video_url = request.form["video_url"]
         yt = YouTube(video_url)
-        ys = yt.streams.get_highest_resolution()
-        ys.download()
+        resolutions = [stream.resolution for stream in yt.streams.filter(progressive=True)]
 
-        return send_file(f"{yt.title}.mp4", as_attachment=True)
-
-    return render_template("index.html")
+    # if request.method == "POST" and 'download_resolution' in request.form:
+    #     yt = YouTube(video_url)
+    #     ys = yt.streams.filter(res=f"{resolution}").first()
+    #     if ys:
+    #         title = yt.title
+    #         file_path = f"{title}.mp4"
+    #         ys.download(filename=title)
+    #         return send_file(file_path, as_attachment=True)
+        
+    return render_template("index.html", resolutions=resolutions, video_url=video_url)
